7a965b71e89842f19e8bcd22cab3f2cdcf0590dd
Timolai Andrievich <timolai.andrievitch@ya.ru>
2024-04-14 21:52:48 +0300

    Actually added python venv

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Create a python environment
+VENV=$HOME/.venv/
+python3 -m venv $VENV
+source $VENV/bin/activate
+
+# Download the dataset
 url='https://disk.yandex.ru/d/Rb-ui8R5S9u-uA'
 wget "$(yandex-direct $url)" -O $HOME/data/dataset.sql.lzma
 cd $HOME/data/
-lzma -kd dataset.sql.lzma
+
