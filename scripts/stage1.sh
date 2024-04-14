152410961227dcea680d43c1fe022cb45a0f0762
Timolai Andrievich <timolai.andrievitch@ya.ru>
2024-04-14 21:57:22 +0300

    Added tqdm

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,7 +4,7 @@
 VENV=$HOME/.venv/
 python3 -m venv $VENV
 source $VENV/bin/activate
-echo "Using python $(which python3)"
+pip3 install tqdm
 
 # Download the dataset
 # url='https://disk.yandex.ru/d/Rb-ui8R5S9u-uA'
