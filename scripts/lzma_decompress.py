4d853035d499b2204c7881904f15e1d0fc362ad9
Timolai Andrievich <timolai.andrievitch@ya.ru>
2024-04-14 22:29:28 +0300

    Updated lzma script

--- scripts/lzma_decompress.py
+++ scripts/lzma_decompress.py
@@ -3,6 +3,8 @@ import argparse
 import lzma
 import pathlib
 
+import tqdm
+
 
 def existing_file(path):
     path = pathlib.Path(path)
@@ -13,36 +15,55 @@ def existing_file(path):
     return path
 
 
-def not_dir(path):
-    path = pathlib.Path(path)
-    if path.exists() and not path.is_dir():
-        raise FileExistsError(f'{path} is a directory')
-    return path
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input-file', type=existing_file)
+    parser.add_argument('input_file', type=existing_file, metavar='input-file')
     parser.add_argument(
-        'output-file',
-        type=not_dir,
-        required=False,
+        '-o',
+        '--output-file',
         default=None,
     )
+    parser.add_argument('--quiet', action='store_true')
+    parser.add_argument('--chunk_bytes', type=int, default=1024)
+    return parser.parse_args()
 
 
-def decompress_lzma(input_file, output_file, chunk_bytes=1024):
+def decompress_lzma(input_file, output_file, chunk_bytes=1024, quiet=False):
     with (
             lzma.open(input_file) as fin,
             open(output_file, 'wb') as fout,
+            tqdm.tqdm(desc='Decompressing',
+                      unit='B',
+                      unit_divisor=1024,
+                      unit_scale=True,
+                      disable=quiet) as pbar,
     ):
-        while True:
-            data = fin.read(chunk_bytes)
-            if not data:
-                break
-            fout.write(data)
+
+        def chunk_file(file, chunk_bytes):
+            while True:
+                data = file.read(chunk_bytes)
+                if not data:
+                    return
+                yield data
+
+        for chunk in chunk_file(fin, chunk_bytes):
+            fout.write(chunk)
+            pbar.update(chunk_bytes)
 
 
 def main():
     args = parse_args()
-    decompress_lzma(args.input_file, args.output_file)
+    if args.output_file is None:
+        if pathlib.Path(args.input_file).suffix != '.lzma':
+            raise RuntimeError("Couldn't infer the decompressed file name.")
+        args.output_file = pathlib.Path(args.input_file).with_suffix('')
+    decompress_lzma(
+        args.input_file,
+        args.output_file,
+        chunk_bytes=args.chunk_bytes,
+        quiet=args.quiet,
+    )
+
+
+if __name__ == "__main__":
+    main()
