0eff3fc13abc272741706a39e6d1034ba9bf0349
Timolai Andrievich <timolai.andrievitch@ya.ru>
2024-04-14 21:56:41 +0300

    Testing python

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,9 +4,11 @@
 VENV=$HOME/.venv/
 python3 -m venv $VENV
 source $VENV/bin/activate
+echo "Using python $(which python3)"
 
 # Download the dataset
-url='https://disk.yandex.ru/d/Rb-ui8R5S9u-uA'
-wget "$(yadisk-direct $url)" -O $HOME/data/dataset.sql.lzma
-cd $HOME/data/
+# url='https://disk.yandex.ru/d/Rb-ui8R5S9u-uA'
+# compressed_database=$HOME/data/database.sql.lzma
+# wget "$(yadisk-direct $url)" -O $HOME/data/dataset.sql.lzma
+# cd $HOME/data/
 
