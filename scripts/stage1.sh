7bd1c7b1bd598f011032b38d898aa3ae1943bec5
Timolai Andrievich <timolai.andrievitch@ya.ru>
2024-04-15 12:08:16 +0300

    Updated stage 1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Create a python environment
+DATA_DIR=$(pwd)/data
 VENV=$HOME/.venv/
+
+function clear_dir {
+  if [ -d $1 ]
+  then
+    rm -r $1
+  fi
+}
+
+clear_dir($DATA_DIR)
+clear_dir($VENV)
+
+# Create a python environment
 python3 -m venv $VENV
 source $VENV/bin/activate
 pip3 install kaggle > /dev/null
 
-if [ ! -f $HOME/kaggle/kaggle.json ] 
+if [ ! -f $HOME/.kaggle/kaggle.json ] 
 then
   echo "Please put your Kaggle API key into ~/.kaggle/"
   exit 1
 fi
 
 # Download the dataset
-kaggle datasets download -d simiotic/ethereum-nfts -p ./data
-unzip ethereum-nfts.zip
+kaggle datasets download -d simiotic/ethereum-nfts -p $DATA_DIR
+unzip $DATA_DIR/ethereum-nfts.zip
